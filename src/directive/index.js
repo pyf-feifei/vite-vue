41dedec7ed900711505782205d5ede8a8750e1bf
SKATE-F <email@sample.com>
2024-03-01 13:50:09 +0800

    添加元素外部点击事件

--- src/directive/index.js
+++ src/directive/index.js
@@ -1,7 +1,9 @@
 import { testDirective } from './test'
+import { clickOutDirective } from './clickOut'
 
 // 全局注册 directive 方法
 export function setupDirective(app) {
   // 使 v-hasPerm 在所有组件中都可用
   app.directive('testDirective', testDirective)
+  app.directive('clickOutDirective', clickOutDirective)
 }
